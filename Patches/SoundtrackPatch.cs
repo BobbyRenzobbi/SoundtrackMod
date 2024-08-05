e2304c431c94283a9d59af160722a2b52b5a10ce
Rayd Zain <rzain200@gmail.com>
2024-08-05 18:18:29 -0400

    First Commit

---
+++ Patches/SoundtrackPatch.cs
@@ -0,0 +1,58 @@
+﻿using SPT.Reflection.Patching;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EFT;
+using System.Runtime.CompilerServices;
+using SoundtrackMod;
+using UnityEngine;
+using Comfort.Common;
+
+namespace SoundtrackMod.Patches
+
+{
+internal class SoundtrackPatch : ModulePatch
+    {
+        public static string[] GetTrack()
+        {
+
+            return new string[] { "aberration.ogg", "dw_dream.ogg", "dw_escape_from_a_dream.ogg", "dw_new_dawn.ogg", "dw_piotrek.ogg" };
+        }
+        private static float trackLength = 0f;
+        public static Player GetYourPlayer()
+        {
+            GameWorld gameWorld = Singleton<GameWorld>.Instance;
+            return gameWorld.MainPlayer;
+        }
+        public static void PlaySoundtrack(Player player)
+        {
+            string[] clips = GetTrack();
+            if (clips == null) return;
+            int rndNumber = UnityEngine.Random.Range(0, clips.Length);
+            string clip = clips[rndNumber];
+            AudioClip audioClip = Plugin.tracks[clip];
+            trackLength = audioClip.length;
+            Singleton<BetterAudio>.Instance.PlayAtPoint(new Vector3(0, 0, 0), audioClip, 0, BetterAudio.AudioSourceGroupType.Nonspatial, 100, 0.1f, EOcclusionTest.None, null, false);
+        }
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(BaseLocalGame<>), nameof(BaseLocalGame<EftGamePlayerOwner>.method_0));
+        }
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+                return true;
+        }
+
+        [PatchPostfix]
+        static void Postfix()
+        {
+            PlaySoundtrack(GetYourPlayer());
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,81 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using SoundtrackMod.Patches;
+using System.IO;
+using UnityEngine.Networking;
+namespace SoundtrackMod
+{
+    [BepInPlugin("BobbyRenzobbi.SoundtrackMod", "SoundtrackMod", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private async Task<AudioClip> RequestAudioClip(string path)
+        {
+            string extension = Path.GetExtension(path);
+            AudioType audioType = AudioType.WAV;
+            switch (extension)
+            {
+                case ".wav":
+                    audioType = AudioType.WAV;
+                    break;
+                case ".ogg":
+                    audioType = AudioType.OGGVORBIS;
+                    break;
+                case ".mp3":
+                    audioType = AudioType.MPEG;
+                    break;
+            }
+            UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, audioType);
+            UnityWebRequestAsyncOperation sendWeb = uwr.SendWebRequest();
+
+            while (!sendWeb.isDone)
+                await Task.Yield();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Logger.LogError("Soundtrack Mod: Failed To Fetch Audio Clip");
+                return null;
+            }
+            else
+            {
+                AudioClip audioclip = DownloadHandlerAudioClip.GetContent(uwr);
+                return audioclip;
+            }
+        }
+        public static bool HasReloadedAudio = false;
+        public static Dictionary<string, AudioClip> tracks = new Dictionary<string, AudioClip>();
+        public static ManualLogSource LogSource;
+        private async void LoadAudioClips()
+        {
+            string[] musicTracks = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\Soundtrack\\sounds");
+
+            tracks.Clear();
+
+            foreach (string fileDir in musicTracks)
+            {
+                tracks[Path.GetFileName(fileDir)] = await RequestAudioClip(fileDir);
+            }
+            Plugin.HasReloadedAudio = true;
+        }
+
+        private void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+            try
+            {
+                LoadAudioClips();
+            }
+            catch (Exception exception)
+            {
+                Logger.LogError(exception);
+            }
+            new SoundtrackPatch().Enable();
+        }
+    }
+}
