fb29ffafa697a70ccfb7637e1cb3d18dcd1a6c30
Rayd Zain <rzain200@gmail.com>
2024-08-05 19:07:06 -0400

    Added Shuffle function. Taken from SamSWAT's CustomMusic

--- Patches/SoundtrackPatch.cs
+++ Patches/SoundtrackPatch.cs
@@ -11,15 +11,19 @@ using System.Runtime.CompilerServices;
 using SoundtrackMod;
 using UnityEngine;
 using Comfort.Common;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace SoundtrackMod.Patches
 
 {
-internal class SoundtrackPatch : ModulePatch
+    internal class SoundtrackPatch : ModulePatch
     {
+        private static string[] newClips;
+        private static bool _replaceOriginal;
         public static string[] GetTrack()
         {
-
+            newClips = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\Soundtrack\\sounds");
             return new string[] { "aberration.ogg", "dw_dream.ogg", "dw_escape_from_a_dream.ogg", "dw_new_dawn.ogg", "dw_piotrek.ogg" };
         }
         private static float trackLength = 0f;
@@ -42,11 +46,21 @@ internal class SoundtrackPatch : ModulePatch
         {
             return AccessTools.Method(typeof(BaseLocalGame<>), nameof(BaseLocalGame<EftGamePlayerOwner>.method_0));
         }
+        private static void Shuffle(string[] clips)
+        {
+            int n = clips.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = UnityEngine.Random.Range(0, n + 1);
+                (clips[k], clips[n]) = (clips[n], clips[k]);
+            }
+        }
 
         [PatchPrefix]
         static bool Prefix()
         {
-                return true;
+            return true;
         }
 
         [PatchPostfix]
