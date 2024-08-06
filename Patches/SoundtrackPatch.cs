19460e5d4cb7a3e155d1a77f740b23c9fc870b10
Rayd Zain <rzain200@gmail.com>
2024-08-05 22:38:50 -0400

    `PlaySoundtrack` method now calls itself after sleeping for the duration of the played track

--- Patches/SoundtrackPatch.cs
+++ Patches/SoundtrackPatch.cs
@@ -11,48 +11,46 @@ using System.Runtime.CompilerServices;
 using SoundtrackMod;
 using UnityEngine;
 using Comfort.Common;
+using System.Threading;
 
 namespace SoundtrackMod.Patches
-
 {
-internal class SoundtrackPatch : ModulePatch
+    internal class SoundtrackPatch : ModulePatch
     {
         public static string[] GetTrack()
         {
 
             return new string[] { "aberration.ogg", "dw_dream.ogg", "dw_escape_from_a_dream.ogg", "dw_new_dawn.ogg", "dw_piotrek.ogg" };
         }
+        private static readonly string[] clips = GetTrack();
         private static float trackLength = 0f;
         public static Player GetYourPlayer()
         {
             GameWorld gameWorld = Singleton<GameWorld>.Instance;
             return gameWorld.MainPlayer;
         }
-        public static void PlaySoundtrack(Player player)
+        public static void PlaySoundtrack()
         {
-            string[] clips = GetTrack();
             if (clips == null) return;
             int rndNumber = UnityEngine.Random.Range(0, clips.Length);
             string clip = clips[rndNumber];
             AudioClip audioClip = Plugin.tracks[clip];
             trackLength = audioClip.length;
             Singleton<BetterAudio>.Instance.PlayAtPoint(new Vector3(0, 0, 0), audioClip, 0, BetterAudio.AudioSourceGroupType.Nonspatial, 100, 0.1f, EOcclusionTest.None, null, false);
+            Logger.LogInfo("Playing " + clip);
+            Thread.Sleep(Convert.ToInt32((trackLength) * 1000));
+            PlaySoundtrack();
         }
+        static Thread t1 = new Thread(new ThreadStart(PlaySoundtrack));
         protected override MethodBase GetTargetMethod()
         {
             return AccessTools.Method(typeof(BaseLocalGame<>), nameof(BaseLocalGame<EftGamePlayerOwner>.method_0));
         }
 
-        [PatchPrefix]
-        static bool Prefix()
-        {
-                return true;
-        }
-
         [PatchPostfix]
         static void Postfix()
         {
-            PlaySoundtrack(GetYourPlayer());
+            t1.Start();
         }
     }
 }
