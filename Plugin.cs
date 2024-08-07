9ebc44a91bba8d225c4a88489091b9f33500f2f3
Rayd Zain <rzain200@gmail.com>
2024-08-07 03:17:54 -0400

    Removed `SoundtrackPatch.cs`. Fixed issue that cause the second raid to call `PlaySoundtrack()` every time `Update()` was called.

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using SoundtrackMod.Patches;
 using System.IO;
 using UnityEngine.Networking;
 using BepInEx.Configuration;
@@ -64,14 +63,24 @@ namespace SoundtrackMod
             {
                 tracks[Path.GetFileName(fileDir)] = await RequestAudioClip(fileDir);
             }
-            Plugin.HasReloadedAudio = true;
+            HasReloadedAudio = true;
         }
-
+        private static string[] clips;
+        public static string[] GetTrack()
+        {
+            return Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\Soundtrack\\sounds");
+        }
+        private static float trackLength = 0f;
+        public static void PlaySoundtrack()
+        {
+            
+        }
         private void Awake()
         {
+            clips = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\Soundtrack\\sounds").Select(file => Path.GetFileName(file)).ToArray();
             string settings = "Soundtrack Settings";
 
-            musicVolume = Config.Bind<float>(settings, "In-raid music volume", 0.025f, new ConfigDescription("This slider will control the volume of the music heard in raid", new AcceptableValueRange<float>(0.001f, 1f)));
+            musicVolume = Config.Bind<float>(settings, "In-raid music volume", 0.025f, new ConfigDescription("Volume of the music heard in raid (This currently does not affect a track if it is already playing)", new AcceptableValueRange<float>(0.001f, 1f)));
 
             LogSource = Logger;
             LogSource.LogInfo("plugin loaded!");
@@ -83,28 +92,62 @@ namespace SoundtrackMod
             {
                 Logger.LogError(exception);
             }
-            new SoundtrackPatch().Enable();
         }
 
         private static float trackTimer = 0f;
+        private static int rndNumber = 0;
+        private static string clip = "";
 
         private void Update()
         {
-            if (Singleton<GameWorld>.Instance != null)
+
+            if (Singleton<GameWorld>.Instance == null)
             {
-                trackTimer += Time.deltaTime;
-                if (trackTimer >= SoundtrackPatch.trackLength)
-                {
-                    trackTimer = 0f;
-                    SoundtrackPatch.PlaySoundtrack();
-                }
+                HasReloadedAudio = false;
+                trackTimer = 0f;
+                trackLength = 0f;
             }
             else
             {
-                trackTimer = 0f;
-                SoundtrackPatch.trackLength = 0f;
+
+                if (!HasReloadedAudio)
+                {
+                    try
+                    {
+                        LoadAudioClips();
+                        HasReloadedAudio = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        LogSource.LogError(ex);
+                    }
+                }
+                trackTimer += Time.deltaTime;
+                if (trackTimer > trackLength)
+                {
+                    LogSource.LogInfo("trackTimer: " + trackTimer + " was greater than trackLength: " + trackLength);
+                    if (clips == null)
+                    {
+                        LogSource.LogInfo("No audio files found");
+                        return;
+                    }
+                    rndNumber = UnityEngine.Random.Range(0, clips.Length);
+                    LogSource.LogInfo("Random number selected");
+                    clip = clips[rndNumber];
+                    LogSource.LogInfo("Random clip selected");
+                    AudioClip audioClip = tracks[clip];
+                    LogSource.LogInfo("audioClip updated");
+                    Singleton<BetterAudio>.Instance.PlayAtPoint(new Vector3(0, 0, 0), audioClip, 0, BetterAudio.AudioSourceGroupType.Nonspatial, 100, musicVolume.Value, EOcclusionTest.None, null, false);
+                    LogSource.LogInfo("Playing " + clip);
+                    trackLength = audioClip.length;
+                    LogSource.LogInfo("trackLength updated");
+                    trackTimer = 0f;
+                }
+                else
+                {
+                    LogSource.LogInfo("trackTimer is only at " + trackTimer + " while trackLength is at " + trackLength);
+                }
             }
         }
-
     }
 }
